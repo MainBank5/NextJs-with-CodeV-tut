1652a29394b2016c1a626a822acc4134eeaa52d5
eliudkaruga <eliudkaruga97@gmail.com>
2024-01-20 18:55:42 +0300

    typescript update

--- my-app/src/app/products/[productId]/page.tsx
+++ my-app/src/app/products/[productId]/page.tsx
@@ -1,3 +1,9 @@
-export default function Productdetails ({params}:{params:{productId: string}}) {
+type Productdetsprops = {
+    params: {
+        productId?: string;
+    }
+}
+
+export default function Productdetails ({params}:Productdetsprops) {
     return <h2>Details about this product {params.productId}</h2>
 }
