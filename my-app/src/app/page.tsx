7bfbf1337c92aa4f14c470081f8f1d1951e60f6e
eliudkaruga <eliudkaruga97@gmail.com>
2024-01-29 13:49:01 +0300

    link navigation

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,3 +1,13 @@
+import Link from 'next/link'
+
+
 export default function Home () {
-    return <h1 className="text-3xl text-red-500">Welcome Home </h1>;
+    return(
+        <>
+
+        <h1 className="text-3xl text-red-500">Welcome Home </h1>
+        <Link href="/blog">Blog</Link>
+        <Link href="/products">Product</Link>
+        </>
+    ) 
 }
--- my-app/src/app/products/page.tsx
+++ my-app/src/app/products/page.tsx
@@ -1,11 +1,17 @@
+import Link from 'next/link'
+
 export default function Products () {
+    const productId = 100
     return (
         <>
         <h1>Product list</h1>
+       <Link href="/">Home</Link>
         <ul>
-            <li>Product 1</li>
-            <li>Product 2</li>
-            <li>Product 3</li>
+            <li><Link href="products/1">Product 1</Link></li>
+            <li><Link href="products/2">Product 2</Link></li>
+            <li><Link href="products/3">Product 3</Link></li>
+            <li><Link href={`products/${productId}`}>Product {productId}</Link></li>
+            
         </ul>
         </>
     )
